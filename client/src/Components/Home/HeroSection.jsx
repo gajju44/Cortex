76e19f5b2908741051770fa5c41c7680481a7fcb
gajju44 <gajjunaphade@gmail.com>
2025-02-19 01:41:53 +0530

    .

--- client/src/Components/Home/HeroSection.jsx
+++ client/src/Components/Home/HeroSection.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import Navbar from '../Navbar/Navbar'
 import Iridescence from './Iridescence'
-import Carousel from './Carousel'
 function HeroSection() {
   return (
     <>
